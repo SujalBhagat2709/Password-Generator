06c2ed0cd51daa3fcf3c83eb2c6582246ab8efba
Sujal Bhagat <167618538+SujalBhagat2709@users.noreply.github.com>
2026-04-26 02:36:10 +0530

    Add files via upload

---
+++ generate.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+def generate_password(length=8):
+    
+    chars = string.ascii_letters + string.digits
+    
+    password = "".join(random.choice(chars) for _ in range(length))
+    
+    return password
+
+
+if __name__ == "__main__":
+    
+    print(generate_password())
